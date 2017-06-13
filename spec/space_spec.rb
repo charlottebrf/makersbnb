e732cb600cf2ab6abeb2518e8385973dda02d1a7
Simon Ashbery <si.ashbery@gmail.com>
2017-06-13 10:57:06 +0100

    Uncommented the posting space feature spec

--- spec/space_spec.rb
+++ spec/space_spec.rb
@@ -1,13 +1,13 @@
-# feature "Listing Spaces" do
-#
-#   scenario "I can create a new Space" do
-#
-#     visit("/spaces")
-#     expect(page).to_not have_content("Space #1")
-#     click_button("make_new_space")
-#     click_button("submit")
-#     expect(page).to have_content("Space #1")
-#
-#   end
-#
-# end
+feature "Listing Spaces" do
+
+  scenario "I can create a new Space" do
+
+    visit("/spaces")
+    expect(page).to_not have_content("Space #1")
+    click_button("make_new_space")
+    click_button("submit")
+    expect(page).to have_content("Space #1")
+
+  end
+
+end
