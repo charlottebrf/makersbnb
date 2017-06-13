805fd74d1b5c3717ea30841feb741d02693bf47d
Charlotte Fereday <cbrfereday@gmail.com>
2017-06-13 12:09:33 +0100

    Fixes erroneous test from web_helper

--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -1,7 +1,7 @@
 
 def new_space
   visit("/spaces")
-  expect(page).to_not have_content("Space #1")
+  expect(page).to_not have_content("Amazingly Romantic Shed")
   click_button("make_new_space")
   fill_in 'name', with: 'Amazingly Romantic Shed'
   click_button("submit")
