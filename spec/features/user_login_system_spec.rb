854b8af5adabfb7324d8ee3a05e6051989be9120
wemmm <jenny.wem@gmail.com>
2017-06-14 15:53:24 +0100

    Adds more robust tests for password verification and login/logout

--- spec/features/user_login_system_spec.rb
+++ spec/features/user_login_system_spec.rb
@@ -1,5 +1,11 @@
-# login
-#
-# password validation
-#
-# logout
+require 'web_helper'
+
+feature "User Login System" do
+
+    scenario "I can log in and out after I have signed up" do
+      sign_up
+      click_button 'Log Out'
+      sign_in
+      expect(page).to have_content "Hello, Jenny!"
+    end
+end
